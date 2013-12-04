cf4e8a2018a2436567bd3854873388d5099fb109
Rob Friesel <rob@founddrama.net>
2013-12-03 22:44:40 -0500

    adding chapter 3 recipe 2 code   - isn't that just like me? starting in the middle...

---
+++ chapter03/recipe02.js
@@ -0,0 +1,12 @@
+var page = require('webpage').create();
+
+page.open('http://blog.founddrama.net/', function(status) {
+  if (status === 'success') {
+    console.log('webpage opened successfully');
+    phantom.exit(0);
+  } else {
+    // status === 'fail'
+    console.error('webpage did not open successfully');
+    phantom.exit(1);
+  }
+});
