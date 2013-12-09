5293fe312693a765920408df3693ed33281dbc7c
Rob Friesel <rob@founddrama.net>
2013-12-08 20:17:54 -0500

    Ch. 1 Recipe 6 script...

--- chapter01/recipe06.js
+++ chapter01/recipe06.js
@@ -1,4 +1,7 @@
-var page = require('webpage').create();
+/*jshint phantom:true, devel:true */
+var page  = require('webpage').create(),
+    count = 0,
+    until = 2;
 
 page.onResourceReceived = function(res) {
   if (res.stage === 'end') {
@@ -16,12 +19,24 @@ page.onResourceReceived = function(res) {
   }
 };
 
-page.open('http://localhost:3000/cache-demo', function(status) {
+page.onLoadStarted = function() {
+  count += 1;
+  console.log('Run ' + count + ' of ' + until + '.');
+};
+
+page.onLoadFinished = function(status) {
   if (status === 'success') {
-    console.log('All done.');
-    phantom.exit();
+    if (count < until) {
+      console.log('Go again.\n');
+      page.reload();
+    } else {
+      console.log('All done.');
+      phantom.exit();
+    }
   } else {
     console.error('Could not open page! (Is it running?)');
     phantom.exit(1);
   }
-});
+};
+
+page.open('http://localhost:3000/cache-demo');
