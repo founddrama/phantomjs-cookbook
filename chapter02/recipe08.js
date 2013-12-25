07efe9a80e68f640f6c3cdb4d9a22d870f5de4f5
Rob Friesel <rob@founddrama.net>
2013-12-24 22:14:48 -0500

    writing a file recipe for ch.2

---
+++ chapter02/recipe08.js
@@ -0,0 +1,21 @@
+/*jshint devel:true, phantom:true */
+
+var fs        = require('fs'),
+    targetDir = 'foo-log';
+
+if (!fs.exists(targetDir)) {
+  console.log('Creating directory ' + targetDir);
+  fs.makeDirectory(targetDir);
+}
+
+if (!fs.isWritable(targetDir)) {
+  console.error(targetDir + ' is not writable!');
+  phantom.exit(1);
+}
+
+console.log('Writing file...');
+var currentTime = new Date().getTime();
+fs.write(targetDir + fs.separator + currentTime + '.txt',
+  'Current time is ' + currentTime, 'w');
+
+phantom.exit();
