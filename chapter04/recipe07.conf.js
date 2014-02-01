c878a669ec89a3cd02cffa5c12a4b71be344894f
Rob Friesel <rob@founddrama.net>
2014-01-31 22:51:42 -0500

    recipes 6 + 7 for Ch. 4

--- chapter04/recipe07.conf.js
+++ chapter04/recipe07.conf.js
@@ -2,7 +2,15 @@ module.exports = function(config) {
   config.set({
     frameworks: ['jasmine'],
     files: ['../lib/string-utils*.js'],
+    preprocessors: {
+      '../lib/string-utils.js': 'coverage'
+    },
     port: 9876,
+    reporters: ['progress', 'coverage'],
+    coverageReporter: {
+      type: 'html',
+      dir: 'coverage/'
+    },
     colors: true,
     browsers: ['PhantomJS'],
     captureTimeout: 60000,
