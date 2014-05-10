192375d53b5b06694af8ac636f295ff2df6cd899
Rob Friesel <rob@founddrama.net>
2014-05-10 08:19:46 -0400

    Ch.4 Tech Edit feedback change to code

--- chapter04/recipe06.conf.js
+++ chapter04/recipe06.conf.js
@@ -1,7 +1,10 @@
 module.exports = function(config) {
   config.set({
     frameworks: ['jasmine'],
-    files: ['../lib/string-utils.js', '../lib/string-utils-spec.js'],
+    files: [
+      '../lib/string-utils.js',
+      '../lib/string-utils-spec.js'
+    ],
     browsers: ['PhantomJS'],
     singleRun: true
   });
