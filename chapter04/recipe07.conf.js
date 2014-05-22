3b40e065470343a3e9d3083d5e4ffc773a60e027
Rob Friesel <rob@founddrama.net>
2014-05-21 23:04:44 -0400

    fix for Karma conf Ch.4#7

--- chapter04/recipe07.conf.js
+++ chapter04/recipe07.conf.js
@@ -1,7 +1,10 @@
 module.exports = function(config) {
   config.set({
     frameworks: ['jasmine'],
-    files: ['../lib/string-utils*.js'],
+    files: [
+      '../lib/string-utils.js',
+      '../lib/string-utils-spec.js'
+    ],
     preprocessors: {
       '../lib/string-utils.js': 'coverage'
     },
